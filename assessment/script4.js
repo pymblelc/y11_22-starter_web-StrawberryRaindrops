fd2853a9f07bfe749ed34d6fe078519b5d54a138
StrawberryRaindrops <106944378+StrawberryRaindrops@users.noreply.github.com>
2022-07-18 23:03:29 +1000

    syncing just in case

--- assessment/script4.js
+++ assessment/script4.js
@@ -1,9 +1,4 @@
-// let myImage = document.getElementById("myPhoto");
-// let myButton = document.getElementById("btnAnalyse");
-// let results = document.getElementById("myText");
-
-// let imageURL = myImage.src; <- this was how to get the img then send it through the api
-//the webcam thingy
+//these are for the camera bits
 let startCameraBtn = document.getElementById("startCamera");
 let stopCameraBtn = document.getElementById("stopCamera");
 let takePhotoBtn = document.getElementById("takePhoto");
@@ -11,21 +6,16 @@ let myWebcam = document.getElementById("webcam");
 let myCanvas = document.getElementById("canvas");
 let ctx = myCanvas.getContext("2d");
 let submitBtn = document.getElementById("submit");
-let webcam = new Webcam(myWebcam, "user", myCanvas); 
+let webcam = new Webcam(myWebcam, "user", myCanvas);
 
-var haircolor;
-var lipMakeup;
-var glasses;
-
-function showDiv(){
+function showDiv() {
   document.getElementById("boxId").style.display = "block";
-  console.log("block");
-}; 
-function hideDiv(){
+  console.log("hidden camera and shown results");
+}
+function hideDiv() {
   document.getElementById("cameraId").style.display = "none";
   console.log("hide");
-}; 
-
+}
 
 startCameraBtn.addEventListener("click", function () {
   webcam
@@ -43,83 +33,100 @@ stopCameraBtn.addEventListener("click", function () {
 });
 
 takePhotoBtn.addEventListener("click", function () {
-  var picture = webcam.snap(); 
+  var picture = webcam.snap();
   webcam.stop();
 });
 
 submitBtn.addEventListener("click", function () {
-  myCanvas.toBlob(function(blob) {
+  myCanvas.toBlob(function (blob) {
     ImageAPI.analyseFacesBlob(blob, (data) => {
-      
       for (let i = 0; i < data.length; i++) {
-        haircolor = data[i].faceAttributes.hair.hairColor[0].color;
-        glasses = data[i].faceAttributes.glasses;
-        lipstick = data[i].faceAttributes.makeup.lipMakeup;
         let mask = data[i].faceAttributes.occlusion.mouthOccluded;
 
         if (i == 1) {
-          console.log("too many people")
+          console.log("too many people");
           document.location.href = "error.html";
-        }else{
-          console.log("1 person")
+        } else {
+          console.log("1 person");
         }
         if (mask == true) {
-          console.log("wearing a mask")
+          console.log("wearing a mask");
           document.location.href = "error.html";
         }
         console.log(data);
       }
-        
-      });
     });
   });
-  
-  getResults.addEventListener("click", function () {
-    
-    function topText (number) {
-      let hairText = hair[number].text; 
-      let hairImg = hair[number].image;
-      document.getElementById("hairImg").src = hairImg
-      textBox2.innerHTML += hairText;
-      document.getElementById("boxId").style.display = "show";
-    }
-    
-    if (haircolor == "black") {
-      topText(0)
-    }
-    if (haircolor == "blond") {
-      topText(1)
-    }
-    if (haircolor == "brown") {
-      topText(2)
-    }       
-    if (haircolor == "red") {
-      topText(3)
-    }
-    if (haircolor == "unknown") {
-      topText(4)
-    }
-    if (haircolor == "other") {
-      topText(4)
-    }
-    if (haircolor == "white") {
-      topText(5)
-    }
-    if (haircolor == "gray") {
-      topText(5)
-    }
-  
-  
-    if (glasses == "NoGlasses") {
-      let glassesText = pants[0].text;
-      textBox4.innerHTML += glassesText;
-  
-    }
-  // console.log(data[i].faceAttributes.glasses);
-  }
-  );
+});
+// this button goes through the image again to get the results
+getResults.addEventListener("click", function () {
+  myCanvas.toBlob(function (blob) {
+    ImageAPI.analyseFacesBlob(blob, (data) => {
+      for (let i = 0; i < data.length; i++) {
+        let haircolor = data[i].faceAttributes.hair.hairColor[0].color;
+        let glasses = data[i].faceAttributes.glasses;
+        let lipstick = data[i].faceAttributes.makeup.lipMakeup;
 
+        function topText (number) {
+          let hairText = hair[number].text; 
+          let hairImg = hair[number].image;
+          document.getElementById("hairImg").src = hairImg
+          textBox2.innerHTML += hairText;
+        }
+        function pantsText (number) {
+          let pantsText = pants[number].text; 
+          let pantsImg = pants[number].image;
+          document.getElementById("pantsImg").src = pantsImg
+          textBox4.innerHTML += pantsText;
+        }
+        function shoesText (number) {
+          let shoesText = shoes[number].text; 
+          let shoesImg = shoes[number].image;
+          document.getElementById("shoesImg").src = shoesImg
+          textBox6.innerHTML += shoesText;
+        }
 
+        if (haircolor == "black") {
+          topText(0);
+        }
+        if (haircolor == "blond") {
+          topText(1);
+        }
+        if (haircolor == "brown") {
+          topText(2);
+        }
+        if (haircolor == "red") {
+          topText(3);
+        }
+        if (haircolor == "unknown") {
+          topText(4);
+        }
+        if (haircolor == "other") {
+          topText(4);
+        }
+        if (haircolor == "white") {
+          topText(5);
+        }
+        if (haircolor == "gray") {
+          topText(5);
+        }
+        if (glasses == "NoGlasses") {
+          pantsText(0);
+        }
+        if (glasses == "ReadingGlasses") {
+          pantsText(1);
+        }
+        if (lipstick == true) {
+          shoesText(0);
+        }
+        if (lipstick == false) {
+          shoesText(1);
+        }
+        
+      }
+    });
+  });
+});
 
 //this is where i'll write a little description for each top
 hair = [
@@ -136,12 +143,12 @@ hair = [
   {
     hair: "brown",
     text: "This over the shoulder white shirt look is nice for a sunny day but you still want to be stylish. It gives off elegant and mature vibes but also has a modern and chic aesthetic when paired with pants. This top can be substituted with a plain white graphic tshirt if you are uncomfortable with the mature look.",
-   image:"brown",
+    image: "brown",
   },
   {
     hair: "red",
     text: "This is a cute top that suits any weather. The sheer cardigan gives the look at cute and cozy vibe. A nice cafe look or to go out with friends. A substitute for this top would be a white singlet with a bulky cardigen to go for the oversized look.",
-    image:"red.PNG",
+    image: "red.PNG",
   },
   {
     hair: "unknown&other",
@@ -152,33 +159,31 @@ hair = [
     hair: "white&gray",
     text: "This red knitted crop top is a very cute piece to fit a romantic look. This is reminiscent of french fashion which is very clean and sleek. Any red top would be a good substitute if crop tops are not your type. ",
     image: "white_gray.PNG",
-  }
+  },
 ];
-
 
 pants = [
   {
     glasses: "NoGlasses",
-    text: "very minimalistic pants to compliment the top. This also gives an elegant vibe to your outfit which is nice to wear on a date with friends.",
-    image:"noGlasses"
+    text: "These black leather pants are very minimalistic which helps to compliment the top. This also gives an elegant vibe to your outfit which is nice to wear on a date with friends.",
+    image: "noGlasses.PNG",
   },
   {
     glasses: "ReadingGlasses",
     text: "Nice torn black jeans to pair with any top, they are confortable to wear and helps give you outfit a casual vibe.",
-    image:"yesGlasses",
-  }
+    image: "yesGlasses.PNG",
+  },
 ];
-    
+
 shoes = [
   {
-    lip: "true",
+    lipstick: "true",
     text: "White boot heels fit with any outfit, it gives you an extra bit of height and helps highlight the length of your legs.",
-    image: "yesMakeup.PNG,"
+    image: "yesLipstick.PNG,",
   },
   {
-    lip: "false",
+    lipstick: "false",
     text: "plain white sneakers to tie in the outfit and help you stay confortable where ever you go.",
-    image: "noMakeup.PNG",
-  }
+    image: "noLipstick.PNG",
+  },
 ];
-    
