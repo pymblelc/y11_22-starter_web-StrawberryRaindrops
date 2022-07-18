3b735120a819516e8c5f1c71977044031310bab4
StrawberryRaindrops <106944378+StrawberryRaindrops@users.noreply.github.com>
2022-07-18 23:53:51 +1000

    rename and moved stuff around and it works :)

--- assessment/script4.js
+++ assessment/script4.js
@@ -59,11 +59,7 @@ submitBtn.addEventListener("click", function () {
     });
   });
 });
-// //this is the retry button from the error screen that brings you back to the main screen
-// retryBtn.addEventListener("click", function () {
-//   document.location.href = "analysis.html";
-//   console.log("i tired to go back ahahaha")
-// });
+
 // this button goes through the image again to get the results
 getResults.addEventListener("click", function () {
   myCanvas.toBlob(function (blob) {
